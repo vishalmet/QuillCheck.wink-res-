33ce9693972a8ea1f009f9de5ca4cb8066de730c
vishalmet <vishalmet003@gmail.com>
2024-10-25 20:14:20 +0530

    fh

--- src/components/Evaluate/Report.jsx
+++ src/components/Evaluate/Report.jsx
@@ -4,30 +4,30 @@ import Assets from '../Assets';
 const Report = ({ critical, risky, medium, neutral }) => {
     return (
         <div>
-            <div className="space-y-2 text-sm">
-                <div className="flex justify-between space-x-9 items-center">
-                    <div className="flex items-center gap-2">
+            <div className="space-y-2 text-xs sm:text-sm">
+                <div className="flex justify-between space-x-3 sm:space-x-9 items-center">
+                    <div className="flex items-center gap-1 sm:gap-2">
                         <img className='h-4' src={Assets.XVoilet} alt="" />
                         <p>Critical:</p>
                     </div>
                     <p>{critical}</p>
                 </div>
-                <div className="flex justify-between space-x-9 items-center">
-                    <div className="flex items-center gap-2">
+                <div className="flex justify-between space-x-3 sm:space-x-9 items-center">
+                    <div className="flex items-center gap-1 sm:gap-2">
                         <img className='h-4' src={Assets.Caution} alt="" />
                         <p>Risky:</p>
                     </div>
                     <p>{risky}</p>
                 </div>
-                <div className="flex justify-between space-x-9 items-center">
-                    <div className="flex items-center gap-2">
+                <div className="flex justify-between space-x-3 sm:space-x-9 items-center">
+                    <div className="flex items-center gap-1 sm:gap-2">
                         <img className='h-4' src={Assets.Exclamatory} alt="" />
                         <p>Medium:</p>
                     </div>
                     <p>{medium}</p>
                 </div>
-                <div className="flex justify-between space-x-9 items-center">
-                    <div className="flex items-center gap-2">
+                <div className="flex justify-between space-x-3 sm:space-x-9 items-center">
+                    <div className="flex items-center gap-1 sm:gap-2">
                         <img className='h-4' src={Assets.Dot} alt="" />
                         <p>Neutral:</p>
                     </div>
--- src/components/Evaluate/Status.jsx
+++ src/components/Evaluate/Status.jsx
@@ -29,10 +29,10 @@ const Status = ({ totalScore, tokenAge, honeypotStatus, owner }) => {
 
   return (
     <div>
-      <div className="flex sm:gap-1 gap-4 text-xs sm:text-sm">
+      <div className="flex gap-1 sm:gap-4 text-xs sm:text-sm">
         <div className="space-y-3">
           <div className="">
-            <p className='text-[#DDDDDD] text-[8px] sm:text-base'>Last Known Status:</p>
+            <p className='text-[#DDDDDD] text-xs sm:text-base'>Last Known Status:</p>
             <div
               className="h-6 sm:h-10 w-fit rounded-[8px] p-[6px] sm:p-[10px] flex items-center gap-1 sm:gap-2"
               style={{ backgroundColor: honeypotBgColor }} // Set honeypot background color
